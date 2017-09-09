367a0fd9abede002d1b0cee71e2d8a8559d82527
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-09-09 15:36:21 -0400

    finished word object

--- wordcounter.js
+++ wordcounter.js
@@ -1,21 +1,30 @@
 
-var $wordTotal = $(".wordTotal")
-var $submit = $("#submit")
-var $input = $("#fieldText")
+var $wordTotal = $(".wordTotal");
+var $submit = $("#submit");
+var $input = $("#fieldText");
+var $wordCounter = $(".wordCounter");
+var counts = {}
 
 
+$submit.on("click", function(event){
+	event.preventDefault()
+	var words = $input.val()
+	words = words.toLowerCase();
+	words = words.split(" ");
+		for (var i = 0; i < words.length; i++){
+		var word = words[i]
+		if (counts[word] === undefined){
+			counts[word] = 1;
+		} else {
+			counts[word] = counts[word] + 1;
+		}
+	}
 
 
-	$submit.on("click", function(event){
-		event.preventDefault()
-		var newText = $input.val()
-		newText = newText.toLowerCase();
-		newText = newText.split(" ");
-		$wordTotal.empty()
-		$wordTotal.append("Words: " + newText.length);
-	})
+	$wordCounter.empty();
+	$wordCounter.append(counts);
+	$wordTotal.empty();
+	$wordTotal.append("Words: " + words.length);
 
-
-
-
+});
 
